58637d3bd713d3bed126ac724bda0d6917f719dd
Sindre Glomnes <78089936+Sindreglo@users.noreply.github.com>
2025-09-29 12:51:43 +0000

    deploy working :D

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      <h1>Welcome to Next.js (TypeScript)!</h1>
-      <p>This is your home page.</p>
+      <h1>Success!!!</h1>
+      <p>Du er inne.</p>
     </main>
   );
 }
